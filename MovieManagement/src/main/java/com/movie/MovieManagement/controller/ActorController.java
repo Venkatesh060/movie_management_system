418db44d0337d6b845ad0c5ae16b9ba7a33f3bbe
Venkatesh <mekalav159@gmail.com>
2026-03-18 23:02:39 +0530

    feat: complete actor module

--- MovieManagement/src/main/java/com/movie/MovieManagement/controller/ActorController.java
+++ MovieManagement/src/main/java/com/movie/MovieManagement/controller/ActorController.java
@@ -1,4 +1,53 @@
 package com.movie.MovieManagement.controller;
 
+import com.movie.MovieManagement.model.Actor;
+import com.movie.MovieManagement.model.Movie;
+import com.movie.MovieManagement.repository.ActorRepo;
+import com.movie.MovieManagement.repository.MovieRepo;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/actors")
 public class ActorController {
+    private final ActorRepo actorRepo;
+    private final MovieRepo movieRepo;
+
+    public ActorController(ActorRepo actorRepo, MovieRepo movieRepo) {
+        this.actorRepo = actorRepo;
+        this.movieRepo = movieRepo;
+    }
+    @GetMapping("/movies/{movieId}")
+    public List<Actor> getByMovie(@PathVariable Integer movieId){
+        return actorRepo.findByMovieId(movieId);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Actor> getById(@PathVariable Integer id){
+        return actorRepo.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+    @PostMapping
+    public ResponseEntity<?> addActor(@RequestBody Actor actor){
+        if(actor.getMovie()!=null && actor.getMovie().getId()!=null){
+            Movie movie=movieRepo.findById(actor.getMovie().getId())
+                    .orElseThrow(() -> new RuntimeException("Movie Not Found."));
+            actor.setMovie(movie);
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(actorRepo.save(actor));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteActor(@PathVariable Integer id){
+        if(!actorRepo.existsById(id))
+            return ResponseEntity.notFound().build();
+        actorRepo.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+
 }
--- MovieManagement/src/main/java/com/movie/MovieManagement/repository/ActorRepo.java
+++ MovieManagement/src/main/java/com/movie/MovieManagement/repository/ActorRepo.java
@@ -4,6 +4,9 @@ import com.movie.MovieManagement.model.Actor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ActorRepo extends JpaRepository<Actor,Integer> {
+    List<Actor> findByMovieId(Integer movieId);
 }
